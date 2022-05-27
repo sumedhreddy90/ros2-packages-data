bf614378dfaa62a7b3e35686f1127a052796e4ee
sumedhreddy90 <sumedhrk@umd.edu>
2022-05-27 16:33:24 -0400

    dictionary comprehension for hits and packages

--- ros_data/analytics.py
+++ ros_data/analytics.py
@@ -13,23 +13,27 @@ import wordcloud
 
 # dont forget to create 'cache/packages' directory
 # loading package DB file
-# run python scrap.py before running this file to generate DB
 db = MetricDB('packages')
-
-# processing packages data
+# run python scrap.py before running this file to generate DB
 
 package_list = []
+hit_list = []
 packages_query = db.query('SELECT url, hits FROM urls WHERE url LIKE "%galactic%"')
 packages_data = pd.DataFrame(packages_query)
 packages_data = packages_data.reset_index()
 packages_data['url'] = packages_data['url'].map(lambda url: re.sub(r'^.*?/ros', '/ros', url))
+hit_list = packages_data.loc[:15, "hits"].tolist()
 raw_urls = (packages_data.loc[:15,"url"]).tolist()
-print(len(raw_urls))
+
 for i in range(len(raw_urls)):
     package = (''.join(raw_urls[i][:[pos for pos, char in enumerate(raw_urls[i]) if char == '/'][1]+1]))
     package = package.replace("/","")
     package_list.append(package)
 
-print(package_list)
+
+# using dictionary comprehension
+# to convert package_list, hit_list lists to dictionary
+hit_packages_data = {package_list[i]: hit_list[i] for i in range(len(package_list))}
+print(hit_packages_data)
 
 # TODO sort packages upon dowloaded hits
